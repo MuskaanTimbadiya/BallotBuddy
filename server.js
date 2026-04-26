9d343b71050b88fed227046e9e421b3d13891a15
Muskaan Timbadiya <muskaan.timbadiya@gmail.com>
2026-04-26 18:57:40 +0530

    chore: update server port from 3000 to 8080

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware
 app.use(cors());
